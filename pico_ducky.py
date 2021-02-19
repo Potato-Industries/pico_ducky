4cfc1bc2dc07a7cd806613e74b93781159c3fd5d
Potato-Industries <56988989+Potato-Industries@users.noreply.github.com>
2021-02-19 02:19:01 +0000

    Create pico_ducky.py

---
+++ pico_ducky.py
@@ -0,0 +1,11 @@
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+kbd = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(kbd)
+
+kbd.press(Keycode.WINDOWS, Keycode.R)
+kbd.release_all()
+
+layout.write('calc\n')
